147c2fc5c869187477146c571b85b8a442cc7d4e
Feyto <feyto81@gmail.com>
2021-02-09 05:23:17 +0700

    pp

---
+++ resources/views/admin/partials/script.blade.php
@@ -0,0 +1,15 @@
+<script src="{{asset('admin/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('admin/js/popper.min.js')}}"></script>
+    <script src="{{asset('admin/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admin/js/main.js')}}"></script>
+    <script src="{{asset('admin/js/plugins/pace.min.j')}}s"></script>
+    <script type="text/javascript">
+      if(document.location.hostname == 'pratikborsadiya.in') {
+      	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      	ga('create', 'UA-72504830-1', 'auto');
+      	ga('send', 'pageview');
+      }
+    </script>
