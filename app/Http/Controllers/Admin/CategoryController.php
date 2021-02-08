35697238e9b7d1ce32aa26e9368dbf968321606d
Feyto <feyto81@gmail.com>
2021-02-09 05:38:30 +0700

    add perubahan2

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $this->data['categories'] = Category::orderBy('name', 'ASC')->paginate(10);
+        return view('admin.categories.index', $this->data);
+    }
 }
---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.layout')
+@section('title','Categories')
+
+@section('content')
+<div class="app-title">
+  <div>
+    <h1><i class="fa fa-dashboard"></i> Categories</h1>
+    <p>A free and open source Bootstrap 4 admin template</p>
+  </div>
+  <ul class="app-breadcrumb breadcrumb">
+    <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+    <li class="breadcrumb-item"><a href="#">Categories</a></li>
+  </ul>
+</div>
+<div class="row">
+
+</div>
+
+
+@stop
