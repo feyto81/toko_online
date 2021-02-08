9c77d12d5d934c7cc6dd074fa5e4c80166b34993
Feyto <feyto81@gmail.com>
2021-02-09 03:13:24 +0700

    update migration categories

--- database/migrations/2021_02_08_200149_create_categories_table.php
+++ database/migrations/2021_02_08_200149_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->bigInteger('parent_id');
             $table->timestamps();
         });
     }
