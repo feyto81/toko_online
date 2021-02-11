7b9b26265006e980b98a4fead7e149f8c558262f
Feyto <feyto81@gmail.com>
2021-02-11 23:12:48 +0700

    edit Model Product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $this->data['products'] = Product::orderBy('name', 'ASC')->paginate(10);
+        return view('admin.products.index', $this->data);
+    }
 }
---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'sku',
+        'name',
+        'slug',
+        'price',
+        'weight',
+        'length',
+        'widht',
+        'height',
+        'short_description',
+        'description',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function category()
+    {
+        return $this->belongsToMany('App\Models\Category', 'product_categories');
+    }
+
+    public function statuses()
+    {
+        return [
+            0 => 'draft',
+            1 => 'active',
+            2 => 'inactive',
+        ];
+    }
+}
