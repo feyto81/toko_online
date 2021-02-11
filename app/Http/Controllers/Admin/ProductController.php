84ab7f7986437f7fa5831a7d66571b72c8432ab2
Feyto <feyto81@gmail.com>
2021-02-12 00:08:37 +0700

    add file indek productr

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
@@ -12,5 +13,14 @@ class ProductController extends Controller
     {
         $this->data['products'] = Product::orderBy('name', 'ASC')->paginate(10);
         return view('admin.products.index', $this->data);
+    }
+
+    public function create()
+    {
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $this->data['categories'] = $categories->toArray();
+        $this->data['product'] = null;
+        // dd($this->data);
+        return view('admin.products.form', $this->data);
     }
 }
