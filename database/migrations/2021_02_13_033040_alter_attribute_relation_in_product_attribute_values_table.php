3a493ec1f59d8c92b40f77f18c5cf073d62f9aa3
Feyto <feyto81@gmail.com>
2021-02-13 10:34:00 +0700

    add

--- database/migrations/2021_02_13_033040_alter_attribute_relation_in_product_attribute_values_table.php
+++ database/migrations/2021_02_13_033040_alter_attribute_relation_in_product_attribute_values_table.php
@@ -14,7 +14,8 @@ class AlterAttributeRelationInProductAttributeValuesTable extends Migration
     public function up()
     {
         Schema::table('product_attribute_values', function (Blueprint $table) {
-            //
+            $table->dropForeign('product_attribute_values_attribute_id_foreign');
+            $table->foreign('attribute_id')->references('id')->on('attributes');
         });
     }
 
@@ -26,7 +27,8 @@ class AlterAttributeRelationInProductAttributeValuesTable extends Migration
     public function down()
     {
         Schema::table('product_attribute_values', function (Blueprint $table) {
-            //
+            $table->dropForeign('product_attribute_values_attribute_id_foreign');
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
         });
     }
 }
